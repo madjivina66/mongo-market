eac43068fc5bf5eae25e13fe4fbbab4213d803af
Madjivina Zakaria Abatchegue <vidjibusco@gmail.com>
2025-09-25 12:40:58 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5118145902-ea04a",
+  "appId": "1:870908828298:web:ddb21620337475c8402b73",
+  "apiKey": "AIzaSyBcqpvacRbpfZEDbfnzeFdV-dFbVsNDneg",
+  "authDomain": "studio-5118145902-ea04a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "870908828298"
+};
