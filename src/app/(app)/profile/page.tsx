fdf358270edb024cb71a98cb1aba24b2b72139df
Madjivina Zakaria Abatchegue <vidjibusco@gmail.com>
2025-09-29 08:55:24 +0000

    Try fixing this error: `Runtime Error: Error: useState is not defined. E

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,7 +1,7 @@
 
 'use client';
 
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { z } from 'zod';
