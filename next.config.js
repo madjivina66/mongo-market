2ebf91d2fde8b9024a47d6d5ea80bfd33aeb1db5
Madjivina Zakaria Abatchegue <vidjibusco@gmail.com>
2025-10-25 16:54:34 +0000

    PS C:\Users\madji\Desktop\FORMATION MZA-CHAD-DIGITAL\MZA\mongo-market-ma

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -8,6 +9,7 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
