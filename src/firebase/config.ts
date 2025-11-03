51b05d16413204f9f1054ccb9e3cb536a5d44ae4
Madjivina Zakaria Abatchegue <vidjibusco@gmail.com>
2025-11-03 11:56:05 +0000

    j'ai active ca depuis  meme

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:870908828298:web:ddb21620337475c8402b73",
   "apiKey": "AIzaSyBcqpvacRbpfZEDbfnzeFdV-dFbVsNDneg",
   "authDomain": "studio-5118145902-ea04a.firebaseapp.com",
-  "measurementId": "",
+  "measurementId": "G-J6ED4L427F",
+  "storageBucket": "studio-5118145902-ea04a.appspot.com",
   "messagingSenderId": "870908828298"
 };
