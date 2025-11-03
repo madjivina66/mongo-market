b743f8dc12750ac13d0333c3b7cc78fa6d5bb29a
Madjivina Zakaria Abatchegue <vidjibusco@gmail.com>
2025-11-03 12:01:08 +0000

    c'est toujours comme ca

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-5118145902-ea04a",
   "appId": "1:870908828298:web:ddb21620337475c8402b73",
+  "storageBucket": "studio-5118145902-ea04a.appspot.com",
   "apiKey": "AIzaSyBcqpvacRbpfZEDbfnzeFdV-dFbVsNDneg",
   "authDomain": "studio-5118145902-ea04a.firebaseapp.com",
-  "measurementId": "G-J6ED4L427F",
-  "storageBucket": "studio-5118145902-ea04a.appspot.com",
-  "messagingSenderId": "870908828298"
+  "messagingSenderId": "870908828298",
+  "measurementId": "G-J6ED4L427F"
 };
