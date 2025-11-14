d450d2d7d940ef64a84179eddfd568d732afc62c
Madjivina Zakaria Abatchegue <vidjibusco@gmail.com>
2025-11-14 10:40:54 +0000

    CORRIGE MOI CETTE ERREUR

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-5118145902-ea04a",
-  "appId": "1:870908828298:web:ddb21620337475c8402b73",
-  "storageBucket": "studio-5118145902-ea04a.appspot.com",
-  "apiKey": "AIzaSyBcqpvacRbpfZEDbfnzeFdV-dFbVsNDneg",
-  "authDomain": "studio-5118145902-ea04a.firebaseapp.com",
-  "messagingSenderId": "870908828298",
-  "measurementId": "G-J6ED4L427F"
+  projectId: 'studio-5118145902-ea04a',
+  appId: '1:870908828298:web:ddb21620337475c8402b73',
+  storageBucket: 'studio-5118145902-ea04a.appspot.com',
+  apiKey: 'AIzaSyBcqpvacRbpfZEDbfnzeFdV-dFbVsNDneg',
+  authDomain: 'studio-5118145902-ea04a.firebaseapp.com',
+  messagingSenderId: '870908828298',
+  measurementId: 'G-J6ED4L427F',
 };
